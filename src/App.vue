9fc87685c905f40c46d1109a4ead57e207328783
Kurino <gejingze@163.com>
2022-07-31 15:32:19 +0800

    chore: add CD.yml

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,11 @@
 
 <script setup lang="ts">
 import { computed } from 'vue';
-import { useMessage } from 'naive-ui';
 import { useRoute } from 'vue-router';
 const route = useRoute();
 const isLoginPage = computed(() => {
   return route.name == 'login';
 });
-
-window.$message = useMessage();
 </script>
 
 <style scoped></style>
