3eec61e88b865e44789d0df08ba6bf4657cdd1f0
Kurino <gejingze@163.com>
2022-08-02 13:46:24 +0800

    fix: stop propergation

--- src/components/prototype/DraggableArea.vue
+++ src/components/prototype/DraggableArea.vue
@@ -1,66 +1,27 @@
 <template>
   <div ref="area">
-    <Vue3DraggableResizable
-      :initW="110"
-      :initH="120"
-      v-model:x="x"
-      v-model:y="y"
-      v-model:w="w"
-      v-model:h="h"
-      v-model:active="active"
-      :draggable="true"
-      :resizable="true"
-      @activated="print('activated')"
-      @deactivated="print('deactivated')"
-      @drag-start="print('drag-start')"
-      @resize-start="print('resize-start')"
-      @dragging="print('dragging')"
-      @resizing="print('resizing')"
-      @drag-end="print('drag-end')"
-      @resize-end="print('resize-end')"
-    >
-      <n-button class="h-full w-full">hello</n-button>
-    </Vue3DraggableResizable>
-    <Vue3DraggableResizable
-      :draggable="true"
-      :resizable="false"
-      @activated="print('activated')"
-      @deactivated="print('deactivated')"
-      @drag-start="print('drag-start')"
-      @resize-start="print('resize-start')"
-      @dragging="print('dragging')"
-      @resizing="print('resizing')"
-      @drag-end="print('drag-end')"
-      @resize-end="print('resize-end')"
-    >
-      <n-button class="h-full w-full">hello2</n-button>
+    <Vue3DraggableResizable :init-h="1000" :init-w="1000" :draggable="true" :resizable="true">
+      <Vue3DraggableResizable @touchstart.stop="" @mousedown.stop="" :draggable="true" :resizable="true">
+        <login-page />
+      </Vue3DraggableResizable>
+      <Vue3DraggableResizable @touchstart.stop="" @mousedown.stop="" :draggable="true" :resizable="true">
+        <n-button class="h-full w-full">hello2</n-button>
+      </Vue3DraggableResizable>
     </Vue3DraggableResizable>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
+<script setup lang="ts">
+import { ref } from 'vue';
 import Vue3DraggableResizable from 'vue3-draggable-resizable';
-//default styles
 import 'vue3-draggable-resizable/dist/Vue3DraggableResizable.css';
-export default defineComponent({
-  components: { Vue3DraggableResizable },
-  data() {
-    return {
-      x: 100,
-      y: 100,
-      h: 100,
-      w: 100,
-      active: false,
-    };
-  },
-  methods: {
-    print(val: string) {
-      console.log(val);
-    },
-  },
-});
+import LoginPage from '../../views/login/LoginPage.vue';
+function print(val: string) {
+  console.log(val);
+}
+const stopPropergation = (e: Event) => {
+  e.stopPropagation();
+};
 </script>
 <style>
 .parent {
