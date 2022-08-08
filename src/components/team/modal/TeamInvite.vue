aa6e5ac878f6e556eb733e7c8013ee72232f0d56
Makubes <2416013822@qq.com>
2022-08-08 18:51:02 +0800

    复制问题fix

--- src/components/team/modal/TeamInvite.vue
+++ src/components/team/modal/TeamInvite.vue
@@ -26,7 +26,26 @@ const code = ref('');
 const show = ref(false);
 
 const handlePositiveClick = async () => {
-  navigator.clipboard.writeText(code.value);
+  if (navigator.clipboard && window.isSecureContext) {
+    // navigator clipboard 向剪贴板写文本
+    return navigator.clipboard.writeText(code.value);
+  } else {
+    // 创建text area
+    let textArea = document.createElement('textarea');
+    textArea.value = code.value;
+    // 使text area不在viewport，同时设置不可见
+    textArea.style.position = 'absolute';
+    textArea.style.opacity = '0';
+    textArea.style.left = '-999999px';
+    textArea.style.top = '-999999px';
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    // 执行复制命令并移除文本框
+    document.execCommand('copy');
+    textArea.remove();
+  }
+  // navigator.clipboard.writeText(code.value);
 };
 
 function open() {
